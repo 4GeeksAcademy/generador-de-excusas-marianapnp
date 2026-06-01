a1b9efaf3cc3b21855898e9bbd2a3225782120c0
Mariana <navarrosmariana46@gmail.com>
2026-06-01 07:34:11 +0000

    generador listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+excuse.innerHTML = `${generador(who , action , what , when)}`            
+}
+
+const excuse = document.querySelector("#excuse")
+const generador = (who, action, what, when) => {
+  return `${who[Math.floor(Math.random() * who.length)]}
+          ${action[Math.floor(Math.random() * action.length)]}
+          ${what[Math.floor(Math.random() * what.length)]}
+          ${when[Math.floor(Math.random() * when.length)]}`;
+}
+
